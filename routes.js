f5d12aec29d73c6d87286d0fadb6e1039e0b7223
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-08-25 20:24:56 +0530

    made the code cleaner by separating in files

---
+++ routes.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const requestHandler = (req, res) => {
+    if (req.url === '/') {
+        fs.readFile('message.txt' , (err , data)=>{
+            if(err){
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                return res.end('Internal Server Error');
+            }
+
+            let result = '<html><head><title>chat app</title></head>' +
+            '<body><p>' + data +'<form action="/message" method="POST"><input type="text" name="message"/> <button type="submit" >send</button> </form></body></html>'
+            return res.end(result)
+        })
+
+    }
+    if (req.url === '/message' && req.method === 'POST') {
+
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        })
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1]
+            fs.writeFile('message.txt', message , (err)=>{
+                if(err)
+                    console.log(err)
+            })
+            res.statusCode = 302
+            res.setHeader('location', '/')
+            return res.end()
+        })
+
+    }
+};
+
+module.exports = requestHandler;
